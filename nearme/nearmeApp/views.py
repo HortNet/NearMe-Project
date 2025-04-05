53d81109743dbcccfa4b4dcf6da2d0efda4af286
C.Masina <151902823+HortNet@users.noreply.github.com>
2025-04-05 15:12:24 -0700

    mbola man

--- nearme/nearmeApp/views.py
+++ nearme/nearmeApp/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello maza fazasi in there")
-
-# Create your views here.
+    return HttpResponse("Hello world!")
